273eb30450c3fb762c7303d5346de734b1c23f8c
theRealRobG <robertnw5@gmail.com>
2020-11-22 17:13:45 +0000

    SelectMenuUserListTests

--- Sources/SlackBlockKit/BlockElements/SelectMenuUserList.swift
+++ Sources/SlackBlockKit/BlockElements/SelectMenuUserList.swift
@@ -5,7 +5,7 @@
 ///   - `Section`
 ///   - `Actions`
 ///   - `Input`
-public struct SelectMenuUserList: BlockElement, ActionsBlockElement, InputBlockElement, SectionBlockElement {
+public struct SelectMenuUserList: BlockElement, ActionsBlockElement, InputBlockElement, SectionBlockElement, Equatable {
     public static let type = BlockElementType.usersSelect
     /// The type of element. In this case `type` is always `users_select`.
     public let type: String
---
+++ Tests/SlackBlockKitTests/BlockElements/SelectMenuUserListTests.swift
@@ -0,0 +1,26 @@
+import SlackBlockKit
+
+// https://api.slack.com/reference/block-kit/block-elements#users_select__example
+private let userListSelect = """
+{
+    "action_id": "text1234",
+    "type": "users_select",
+    "placeholder": {
+        "type": "plain_text",
+        "text": "Select an item"
+    }
+}
+"""
+
+class SelectMenuUserListTests: BlockElementTestCase {
+    func test_userListSelect() {
+        let expectedSelect = SelectMenuUserList(
+            placeholder: TextObject(
+                type: .plainText,
+                text: "Select an item"
+            ),
+            actionId: "text1234"
+        )
+        testCodableEquality(block: expectedSelect, jsonString: userListSelect)
+    }
+}
