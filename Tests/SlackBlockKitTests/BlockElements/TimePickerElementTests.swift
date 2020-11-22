163ef3a238ee2ddf1603b6c2df0acaf3eb0eb6c0
theRealRobG <robertnw5@gmail.com>
2020-11-22 17:26:41 +0000

    TimePickerElementTests

---
+++ Tests/SlackBlockKitTests/BlockElements/TimePickerElementTests.swift
@@ -0,0 +1,28 @@
+import SlackBlockKit
+
+// https://api.slack.com/reference/block-kit/block-elements#timepicker__example
+private let timePickerElevenForty = """
+{
+    "type": "timepicker",
+    "action_id": "timepicker123",
+    "initial_time": "11:40",
+    "placeholder": {
+        "type": "plain_text",
+        "text": "Select a time"
+    }
+}
+"""
+
+class TimePickerElementTests: BlockElementTestCase {
+    func test_timePickerElevenForty() {
+        let expectedTimePicker = TimePickerElement(
+            actionId: "timepicker123",
+            placeholder: TextObject(
+                type: .plainText,
+                text: "Select a time"
+            ),
+            initialTime: "11:40"
+        )
+        testCodableEquality(block: expectedTimePicker, jsonString: timePickerElevenForty)
+    }
+}
