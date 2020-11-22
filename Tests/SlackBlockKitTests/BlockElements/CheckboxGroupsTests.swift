4e2cb9220fc353ce942188915d7101f0b95858a9
theRealRobG <robertnw5@gmail.com>
2020-11-22 01:46:24 +0000

    CheckboxGroupsTests

---
+++ Tests/SlackBlockKitTests/BlockElements/CheckboxGroupsTests.swift
@@ -0,0 +1,85 @@
+import SlackBlockKit
+import XCTest
+
+// https://api.slack.com/reference/block-kit/block-elements#checkboxes__example
+private let groupOfCheckboxes = """
+{
+    "type": "checkboxes",
+    "action_id": "this_is_an_action_id",
+    "initial_options": [{
+        "value": "A1",
+        "text": {
+            "type": "plain_text",
+            "text": "Checkbox 1"
+        }
+    }],
+    "options": [
+        {
+            "value": "A1",
+            "text": {
+                "type": "plain_text",
+                "text": "Checkbox 1"
+            }
+        },
+        {
+            "value": "A2",
+            "text": {
+                "type": "plain_text",
+                "text": "Checkbox 2"
+            }
+        }
+    ]
+}
+"""
+
+class CheckboxGroupsTests: XCTestCase {
+    var jsonEncoder: JSONEncoder!
+    var jsonDecoder: JSONDecoder!
+    
+    override func setUp() {
+        jsonEncoder = JSONEncoder()
+        jsonDecoder = JSONDecoder()
+    }
+    
+    func test_groupOfCheckboxes() {
+        let expectedCheckbox = CheckboxGroups(
+            actionId: "this_is_an_action_id",
+            options: [
+                OptionObject(
+                    text: TextObject(
+                        type: .plainText,
+                        text: "Checkbox 1"
+                    ),
+                    value: "A1"
+                ),
+                OptionObject(
+                    text: TextObject(
+                        type: .plainText,
+                        text: "Checkbox 2"
+                    ),
+                    value: "A2"
+                )
+            ],
+            initialOptions: [
+                OptionObject(
+                    text: TextObject(
+                        type: .plainText,
+                        text: "Checkbox 1"
+                    ),
+                    value: "A1"
+                )
+            ]
+        )
+        
+        do {
+            // test encode
+            let json = try jsonEncoder.encodeAsString(expectedCheckbox)
+            AssertJSONEqual(json, groupOfCheckboxes)
+            // test decode
+            let checkboxElement = try jsonDecoder.decode(CheckboxGroups.self, from: groupOfCheckboxes)
+            XCTAssertEqual(expectedCheckbox, checkboxElement)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+}
