bbaecf701ef19720bb516857cf85c567e242d098
theRealRobG <robertnw5@gmail.com>
2021-01-02 20:51:49 +0000

    AppHomeOpenedEvent

--- Sources/SlackBlockKit/Events/AppHomeOpenedEvent.swift
+++ Sources/SlackBlockKit/Events/AppHomeOpenedEvent.swift
@@ -1,4 +1,43 @@
-public struct AppHomeOpenedEvent: SlackEvent {
+/// This app event notifies your app when a user has entered the App Home.
+public struct AppHomeOpenedEvent: SlackEvent, Equatable {
     public static let type: SlackEventType = .appHomeOpened
+    /// The type of event. In this case `type` is always `app_home_opened`.
     public let type: String
+    /// The user ID belonging to the user that incited this action. Not included in all events
+    /// as not all events are controlled by users. See the top-level callback object's
+    /// `authed_users` if you need to calculate event visibility by user.
+    public let user: String
+    /// The timestamp of the event. The combination of `event_ts`, `team_id`,
+    /// `user_id`, or `channel_id` is intended to be unique. This field is included
+    /// with every inner event type.
+    public let eventTs: String
+    /// If the user opens a tab within the App Home, the event payload for this event
+    /// will reference that in the `tab` field.
+    public let tab: String?
+    /// If they opened a Home tab and that tab has had a `view` published at least
+    /// once before, a `view` field will also be included. That `view` field will
+    /// contain the current state of the Home tab, including the list of `blocks`,
+    /// and various pieces of metadata.
+    public let view: HomeTabView?
+    
+    public init(
+        user: String,
+        eventTs: String,
+        tab: String? = nil,
+        view: HomeTabView? = nil
+    ) {
+        self.type = Self.type.rawValue
+        self.user = user
+        self.eventTs = eventTs
+        self.tab = tab
+        self.view = view
+    }
+    
+    public enum CodingKeys: String, CodingKey {
+        case type
+        case user
+        case eventTs = "event_ts"
+        case tab
+        case view
+    }
 }
--- Tests/SlackBlockKitTests/Events/AppHomeOpenedEventTests.swift
+++ Tests/SlackBlockKitTests/Events/AppHomeOpenedEventTests.swift
@@ -1,8 +1,72 @@
-//
-//  File.swift
-//  
-//
-//  Created by Robert Galluccio on 02/01/2021.
-//
+import SlackBlockKit
+import XCTest
 
-import Foundation
+let appHomeOpened = """
+{
+  "type": "app_home_opened",
+  "user": "U061F7AUR",
+  "event_ts": "1515449522000016",
+  "tab": "home",
+  "view": {
+    "type": "home",
+    "blocks": [
+      {
+        "accessory": {
+          "action_id": "home-action-1234",
+          "type": "plain_text_input"
+        },
+        "block_id": "home-1234",
+        "text": {
+          "text": "test-text",
+          "type": "plain_text"
+        },
+        "type": "section"
+      }
+    ],
+    "private_metadata": "",
+    "callback_id": "",
+    "state": {
+      "values": {
+        "home-1234": {
+          "home-action-1234": {
+            "type": "plain_text_input",
+            "value": "test-input"
+          }
+        }
+      }
+    },
+    "external_id": ""
+  }
+}
+"""
+
+class AppHomeOpenedEventTests: BlockTestCase {
+    func test_appHomeOpened() throws {
+        var homeTabView = HomeTabView(
+            blocks: [
+                SectionBlock(
+                    text: TextObject(type: .plainText, text: "test-text"),
+                    blockId: "home-1234",
+                    accessory: PlainTextInputElement(actionId: "home-action-1234")
+                )
+            ],
+            privateMetadata: "",
+            callbackId: "",
+            externalId: ""
+        )
+        homeTabView.state = ViewState(
+            values: [
+                "home-1234": [
+                    "home-action-1234": PlainTextInputStateValue(value: "test-input")
+                ]
+            ]
+        )
+        let appHomeOpenedEvent = AppHomeOpenedEvent(
+            user: "U061F7AUR",
+            eventTs: "1515449522000016",
+            tab: "home",
+            view: homeTabView
+        )
+        testCodableEquality(event: appHomeOpenedEvent, jsonString: appHomeOpened)
+    }
+}
